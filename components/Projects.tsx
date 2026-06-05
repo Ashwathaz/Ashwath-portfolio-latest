160af45b0d310c00e583ee529933ee274cc9791a
Ashwathaz <ashwathramj14@gmail.com>
2026-06-05 16:46:58 +0530

    final working i think

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -15,6 +15,7 @@ export default function Projects() {
   
   const [emblaRef, emblaApi] = useEmblaCarousel({ 
     align: 'start',
+    loop: true,
     dragFree: true,
     containScroll: 'trimSnaps'
   });
